4bdd8ae71cb5d38f61325109237944ab3fe25aa9
Laucalle <laucalle09@correo.ugr.es>
2017-06-09 21:44:53 +0200

    Minor fixes

--- Proyecto/proyecto.R
+++ Proyecto/proyecto.R
@@ -29,6 +29,7 @@ evaluar_modelo = function(regresion,datos){
     
 }
 
+# Porcentaje de error utilizando la matriz de confusión
 porcentaje_error = function(clasificados,reales,fp=1,fn=1){
     
     reales[reales == 0] = -1
@@ -40,6 +41,7 @@ porcentaje_error = function(clasificados,reales,fp=1,fn=1){
     
 }
 
+# Pasa de etiquetas en un rango a etiquetas 1 y -1
 categorizar = function(clasificados,umbral=0.5){
     
     clasificados[clasificados < umbral] = -1
@@ -56,6 +58,7 @@ evalua_lm = function(formula,datos,subconjunto,fp=1,fn=1){
     list(formula=formula, error = porc_error)
 }
 
+# Evalúan un cierto modelo de regresión dada una fórmula y unos datos de entrenamiento 
 evalua_glm = function(formula,datos,subconjunto,fp=1,fn=1,familia=binomial()){
     reg_lin = do.call("glm", list(formula=formula, data=substitute(datos), subset=substitute(subconjunto),familia))
     prediccion_test = evaluar_modelo(reg_lin,datos[-subconjunto,-ncol(datos)])
@@ -63,6 +66,7 @@ evalua_glm = function(formula,datos,subconjunto,fp=1,fn=1,familia=binomial()){
     list(formula=formula, error=porc_error,reg=reg_lin)
 }
 
+# Evalúa un modelo de Random Forest
 evalua_random_forest = function(datos,subconjunto,arboles=100){
     rf = do.call("randomForest",list(formula=etiquetas~.,data=substitute(datos),subset=substitute(subconjunto),ntree=arboles,mtry=sqrt(ncol(datos)-1)))
     rf_pred = evaluar_modelo(rf,datos[-subconjunto,-ncol(datos)])
@@ -153,12 +157,15 @@ points(x=glm_sin_pca_min_error_index, y=ajustes_glm_sin_pca[2,glm_sin_pca_min_er
 points(x=1:ncol(ajustes_glm),y=ajustes_glm[2,],type="o",pch=20,col="red")
 points(x=glm_min_error_index, y=ajustes_glm[2,glm_min_error_index],pch=19,col="green")
 legend(16.5,29,c("Sin PCA","Con PCA"),lty=c(1,1),lwd=c(2.5,2.5),col=c("blue","red"))
+scan()
 
 error_glm = ajustes_glm_sin_pca[2,glm_sin_pca_min_error_index]
 pred_glm_in = predict(ajustes_glm_sin_pca[3,glm_sin_pca_min_error_index], datos_procesados_sin_pca[indices_train, -ncol(datos_procesados_sin_pca)])
 error_glm_in = porcentaje_error(categorizar(unlist(pred_glm_in)), etiquetas[indices_train])
 
-
+print(c("Error test regresión logística sin PCA: ", error_glm))
+print(c("Error train regresión logística sin PCA: ", error_glm_in))
+scan()
 
 ###############################################################################
 # Modelos no lineales
@@ -183,25 +190,21 @@ error_rf = porcentaje_error(as.numeric(rf_pred_test),etiquetas[-indices_train])
 
 rf_pred_in = evaluar_modelo(ajustes_rf_cv[3,rf_cv_min_error_index]$rf,datos[indices_train,])
 error_rf_in = porcentaje_error(as.numeric(rf_pred_in),etiquetas[indices_train])
-# rf_pca = evalua_random_forest(datos_procesados,indices_train)
-# rf_sin_pca = evalua_random_forest(datos_procesados_sin_pca,indices_train)
-# 
-# ajustes_rf = mapply(evalua_random_forest,num_arboles,MoreArgs = list(datos=datos_procesados,subconjunto=indices_train))
-# ajustes_rf_sin_pca = mapply(evalua_random_forest,num_arboles,MoreArgs = list(datos=datos_procesados_sin_pca,subconjunto=indices_train))
-# 
-# # Representamos la variación del error en función del número de árboles
-# rf_min_error_index = which.min(unlist(ajustes_rf[2,]))
-# rf_sin_pca_min_error_index = which.min(unlist(ajustes_rf_sin_pca[2,]))
-# plot(x=num_arboles,y=ajustes_rf_sin_pca[2,],pch=20,ylim=c(4,19),type="o",col="blue",xlab="Número de árboles",ylab="Error porcentual", main = "Comparativa de Random Forests")
-# points(x=ajustes_rf_sin_pca[1,rf_sin_pca_min_error_index], y=ajustes_rf_sin_pca[2,rf_sin_pca_min_error_index], pch=19, col="orange")
-# points(x=num_arboles,y=ajustes_rf[2,],type="o",pch=20,col="red")
-# points(x=ajustes_rf[1,rf_min_error_index], y=ajustes_rf[2,rf_min_error_index],pch=19,col="green")
-# legend(67,19,c("Sin PCA","Con PCA"),lty=c(1,1),lwd=c(2.5,2.5),col=c("blue","red"))
 
-# Con tune
-# tuned_rf = tune.randomForest(datos[indices_train,],as.factor(etiquetas[indices_train]),mtry=sqrt(ncol(datos)),ntree=seq(10,100,10))
-# rf_pred_test = evaluar_modelo(tuned_rf$best.model,datos[-indices_train,])
-# error_rf = porcentaje_error(as.numeric(rf_pred_test),etiquetas[-indices_train])
+# Comparativa de los errores de validación cruzada para distinto número de árboles.
+rf_cv_min_error_index = which.min(unlist(ajustes_rf_cv[2,]))
+plot(x=num_arboles,y=ajustes_rf_cv[2,],pch=20,ylim=c(4,19),type="o",col="blue",xlab="Número de árboles",ylab="% Error de validación cruzada", main = "Comparativa de número de árboles")
+points(x=ajustes_rf_cv[1,rf_cv_min_error_index], y=ajustes_rf_cv[2,rf_cv_min_error_index], pch=19, col="orange")
+scan()
+
+# Evolución del error del mejor modelo conforme aumenta el número de árboles
+plot(ajustes_rf_cv[3,rf_cv_min_error_index]$rf$finalModel,lty=1,main="Evolución del mejor Random Forest")
+legend(290,0.25,c("Error Out Of Bag","Error clase 1","Error clase 0"),lty=c(1,1),lwd=c(2.5,2.5),col=c("black","green","red"))
+scan()
+
+print(c("Error test Random Forest: ", error_rf))
+print(c("Error train Random Forest: ", error_rf_in))
+scan()
 
 #######################################
 # Adaboost
@@ -213,6 +216,10 @@ ada_fit = train(x = datos[indices_train,], y = as.factor(etiquetas[indices_train
 ada_pred = predict(ada_fit, datos[-indices_train,])
 error_ada = porcentaje_error(as.numeric(ada_pred), etiquetas[-indices_train])
 
+ada_pred_in = predict(ada_fit, datos[indices_train,])
+error_ada_in = porcentaje_error(as.numeric(ada_pred_in), etiquetas[indices_train])
+
+# Datos para pintar la gráfica a continuación
 arboles_nu = ada_fit$result[which(ada_fit$results[,1] == 0.15)[which.max(ada_fit$results[ada_fit$results[,1] == 0.15,4])],3]
 arboles_nu = c(arboles_nu,ada_fit$result[which(ada_fit$results[,1] == 0.2)[which.max(ada_fit$results[ada_fit$results[,1] == 0.2,4])],3])
 arboles_nu = c(arboles_nu,ada_fit$result[which(ada_fit$results[,1] == 0.25)[which.max(ada_fit$results[ada_fit$results[,1] == 0.25,4])],3])
@@ -224,6 +231,11 @@ datos_nu = rbind(datos_nu,c(0.3, max(ada_fit$results[ada_fit$results[,1] == 0.3,
 
 plot(datos_nu, pch = 20, ylab = "Precisión", xlab = "Coeficiente de aprendizaje", xlim = c(0.13,0.32),col = "blue", type = "o")
 text(x = datos_nu[,1], y = datos_nu[,2] , labels = arboles_nu, cex = 0.7, pos = 2)
+scan()
+
+print(c("Error test AdaBoost: ", error_ada))
+print(c("Error train Random Forest: ", error_ada_in))
+scan()
 
 #######################################
 # Support Vector Machines
@@ -233,6 +245,10 @@ svm_fit = train(x = datos[indices_train,], y = as.factor(etiquetas[indices_train
 svm_pred = predict(svm_fit, datos[-indices_train,])
 error_svm = porcentaje_error(as.numeric(svm_pred), etiquetas[-indices_train])
 
+svm_pred_in = predict(svm_fit, datos[indices_train,])
+error_svm_in = porcentaje_error(as.numeric(svm_pred_in), etiquetas[indices_train])
+
+# Datos para pintar la siguiente gráfica
 c_sigma = svm_fit$result[which(svm_fit$results[,2] == 0.02)[which.max(svm_fit$results[svm_fit$results[,2] == 0.02,3])],1]
 c_sigma = c(c_sigma,svm_fit$result[which(svm_fit$results[,2] == 0.025)[which.max(svm_fit$results[svm_fit$results[,2] == 0.025,3])],1])
 c_sigma = c(c_sigma,svm_fit$result[which(svm_fit$results[,2] == 0.03)[which.max(svm_fit$results[svm_fit$results[,2] == 0.03,3])],1])
@@ -246,20 +262,14 @@ datos_sigma = rbind(datos_sigma,c(0.04, max(svm_fit$results[svm_fit$results[,2] 
 
 plot(datos_sigma, pch = 20, ylab = "Precisión", xlab = "Sigma", xlim = c(0.018,0.042),col = "blue", type = "o")
 text(x = datos_sigma[,1], y = datos_sigma[,2] , labels = c_sigma, cex = 0.7, pos = 2)
+scan()
+
+print(c("Error test Support Vector Machine: ", error_svm))
+print(c("Error train Support Vector Machine: ", error_svm_in))
+scan()
 
 #######################################
 # Neural Networks
-
-# set.seed(17)
-# layers = c(3,3)
-# datos_nn = cbind(datos,etiquetas)
-# colnames(datos_nn)[ncol(datos_nn)] = "etiquetas"
-# formula_nn = as.formula(paste("etiquetas~",paste(colnames(datos),collapse = "+")))
-# net = neuralnet(formula_nn,data=datos_nn[indices_train,],hidden=layers,linear.output=FALSE)
-# prediccion = compute(net,datos[-indices_train,])
-# porcentaje_error(categorizar(prediccion$net.result),etiquetas[-indices_train])
-
-# preProcess = c("YeoJohnson","center","scale")
 
 ptm <- proc.time()
 set.seed(17)
@@ -277,15 +287,28 @@ err_in_una = porcentaje_error(categorizar(p_una_nn_in), etiquetas[indices_train]
 p_una_nn = predict(nn_fit_una, datos[-indices_train,])
 err_una = porcentaje_error(categorizar(p_una_nn), etiquetas[-indices_train])
 
+print(c("Error test Red Neuronal de una capa: ", err_una))
+print(c("Error train Red Neuronal de una capa: ", err_in_una))
+scan()
+
 p_dos_nn_in = predict(nn_fit_dos, datos[indices_train,])
 err_in_dos = porcentaje_error(categorizar(p_dos_nn_in), etiquetas[indices_train])
 p_dos_nn = predict(nn_fit_dos, datos[-indices_train,])
 err_dos = porcentaje_error(categorizar(p_dos_nn), etiquetas[-indices_train])
 
+print(c("Error test Red Neuronal de dos capas: ", err_dos))
+print(c("Error train Red Neuronal de dos capas: ", err_in_dos))
+scan()
+
 p_tres_nn_in = predict(nn_fit_tres, datos[indices_train,])
 err_in_tres = porcentaje_error(categorizar(p_tres_nn_in), etiquetas[indices_train])
 p_tres_nn = predict(nn_fit_tres, datos[-indices_train,])
 err_tres = porcentaje_error(categorizar(p_tres_nn), etiquetas[-indices_train])
+
+print(c("Error test Red Neuronal de tres capas: ", err_tres))
+print(c("Error train Red Neuronal de tres capas: ", err_in_tres))
+scan()
+
 ###############################################################################
 # Curvas ROC
 
@@ -301,24 +324,35 @@ modelo_glm = ajustes_glm_sin_pca[3,glm_sin_pca_min_error_index]
 eval_glm = unlist(evaluar_modelo(modelo_glm,datos_procesados_sin_pca[-indices_train,]))
 roc_glm = calcula_curva_roc(eval_glm,etiquetas[-indices_train])
 plot(roc_glm$curva)
+scan()
+
 area_roc_glm = roc_glm$area@y.values
+
 # Curva ROC Random Forest
 modelo_rf = ajustes_rf_cv[3,rf_cv_min_error_index]
 eval_rf = predict(modelo_rf,datos[-indices_train,],type="prob")$rf[,2]
 roc_rf = calcula_curva_roc(eval_rf,etiquetas[-indices_train])
 plot(roc_rf$curva)
+scan()
+
 area_roc_rf = roc_rf$area@y.values
+
 # Curva ROC AdaBoost
 modelo_ada = ada_fit
 eval_ada = predict(modelo_ada,datos[-indices_train,],type="prob")[,2]
 roc_ada = calcula_curva_roc(eval_ada,etiquetas[-indices_train])
 plot(roc_ada$curva)
+scan()
+
 area_roc_ada = roc_ada$area@y.values
+
 # Curva ROC Support Vector Machines
 modelo_svm = svm_fit
 eval_svm = predict(modelo_svm,datos[-indices_train,],type="prob")[,2]
 roc_svm = calcula_curva_roc(eval_svm,etiquetas[-indices_train])
 plot(roc_svm$curva)
+scan()
+
 area_roc_svm = roc_svm$area@y.values
 
 # Curva ROC Redes Neuronales con una capa
@@ -326,6 +360,8 @@ modelo_nn_una = nn_fit_una
 eval_nn_una = compute(modelo_nn_una$finalModel,datos[-indices_train,])
 roc_nn_una = calcula_curva_roc(eval_nn_una$net.result,etiquetas[-indices_train])
 plot(roc_nn_una$curva)
+scan()
+
 area_roc_nn_una = roc_nn_una$area@y.values
 
 # Curva ROC Redes Neuronales con dos capa
@@ -333,6 +369,8 @@ modelo_nn_dos= nn_fit_dos
 eval_nn_dos = compute(modelo_nn_dos$finalModel,datos[-indices_train,])
 roc_nn_dos = calcula_curva_roc(eval_nn_dos$net.result,etiquetas[-indices_train])
 plot(roc_nn_dos$curva)
+scan()
+
 area_roc_nn_dos = roc_nn_dos$area@y.values
 
 # Curva ROC Redes Neuronales con dos capa
@@ -340,4 +378,15 @@ modelo_nn_tres= nn_fit_tres
 eval_nn_tres = compute(modelo_nn_tres$finalModel,datos[-indices_train,])
 roc_nn_tres = calcula_curva_roc(eval_nn_tres$net.result,etiquetas[-indices_train])
 plot(roc_nn_tres$curva)
+scan()
+
 area_roc_nn_tres = roc_nn_tres$area@y.values
+
+print(c("Área bajo la curva ROC Regresión Logística: ", area_roc_glm))
+print(c("Área bajo la curva ROC Random Forest: ", area_roc_rf))
+print(c("Área bajo la curva ROC AdaBoost: ", area_roc_ada))
+print(c("Área bajo la curva ROC Support Vector Machine: ", area_roc_svm))
+print(c("Área bajo la curva ROC Red Neuronal de una capa: ", area_roc_nn_una))
+print(c("Área bajo la curva ROC Red Neuronal de dos capas: ", area_roc_nn_dos))
+print(c("Área bajo la curva ROC Red Neuronal de tres capas: ", area_roc_nn_tres))
+scan()
